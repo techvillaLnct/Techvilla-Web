9a9a3c5638e42614c4cf3e64674e187172f66a51
udayempire <kuday0726@gmail.com>
2024-11-29 16:32:21 +0530

    added fontys

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,17 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
+// import localFont from "next/font/local";
+import { Michroma } from "next/font/google";
 import "./globals.css";
+import Link from "next/link"
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
+const michroma = Michroma({
+  weight: "400", // Specify the font weight you need
+  subsets: ["latin"], // Character subset
 });
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+
 
 export const metadata: Metadata = {
-  title: "Techvilla",
+  title: "TechVilla",
   description: "Techvilla is Coding Club created in 2023 in LNCT 1994 to empower coding among students",
 };
 
@@ -24,10 +21,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={michroma.className} >
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={` antialiased`}
       >
+        <Link href={"https://lu.ma/f6pqfzl8"} className="flex justify-center bg-[#FF8B12] font-semibold py-1  ">
+          JOIN US FOR OUR ORIENTATION 2024-25 !!
+        </Link>
         {children}
       </body>
     </html>
