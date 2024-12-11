7336475b555d8c3db9783a97da95add06e16356e
udayempire <kuday0726@gmail.com>
2024-12-12 03:56:36 +0530

    fixed last commit

--- components/UpcomingEvents.tsx
+++ components/UpcomingEvents.tsx
@@ -4,15 +4,15 @@ import { useRouter } from "next/navigation"
 import {inter} from '../app/fonts'
 export const UpcomingEvent = () => {
     const router = useRouter()
-    return <div className={` ${inter.className} w-[22rem] md:w-1/2 flex flex-col  gap-4 border  rounded-md backdrop-blur-lg`}>
+    return <div className={` ${inter.className} w-[20rem] md:w-1/2 flex flex-col  gap-4 border  rounded-md backdrop-blur-lg`}>
 
         <Image src={"/codebeta2.0.png"} className="rounded-md  " width={1500} height={0} alt=""  ></Image>
         <h1 className={` ${inter.className} text-center text-xl font-bold tracking-wider`}>CodeBeta 2.0</h1>
-        <p className={` ${inter.className} text-center   px-4 text-lg font-medium`} >Join Us For Most Awaiting Coding Competition - CODEBETA 2.0</p>
+        <p className={` ${inter.className} text-center   px-4 text-md font-medium`} >Join Us For Most Awaiting Coding Competition - CODEBETA 2.0</p>
         <div className="flex justify-between gap-2">
             <div className="flex gap-2 px-8">
                 <Image src={"/Location.svg"} alt="" width={28} height={28} ></Image>
-                <p>G-01 , LNCT</p>
+                <p className="w-auto">G-01 , LNCT</p>
             </div> 
             <div className="flex gap-2  px-8">
                 <Image src={"/date.svg"} alt="" width={28} height={28} ></Image>
