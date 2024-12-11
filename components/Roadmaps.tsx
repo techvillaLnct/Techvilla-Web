bcb8251e75a1e1ded9ce9c3227eb9f79e607ec2e
udayempire <kuday0726@gmail.com>
2024-12-12 05:18:42 +0530

    updated resources design

--- components/Roadmaps.tsx
+++ components/Roadmaps.tsx
@@ -13,13 +13,13 @@ interface Roadmaps {
 export const Roadmaps = ({ title, desc, img, link }: Roadmaps) => {
     const router = useRouter()
     return (
-        <div className="relative w-80 md:w-96 md:h-96 overflow-hidden rounded-lg p-[1px] backdrop-blur-2xl shadow-2xl border-white">
+        <div className="relative w-80 md:w-96 md:h-96 overflow-hidden rounded-lg p-[1px]  backdrop-blur-2xl shadow-2xl border border-white ">
             <div className="relative flex h-full w-full flex-col gap-2 rounded-lg bg-black">
                 <Image alt="" src={img} width={500} height={500} className="rounded-b-sm object-cover w-full h-48 " />
                 <p className="font-semibold text-center text-white">{title}</p>
                 <div className="p-4">
                     <p className={`${geistVF.className} text-lg text-left text-white`}>{desc}</p>
-                    <div className="flex justify-center mt-4">
+                    <div className="flex justify-center mt-4 ">
                         <Button2 onClick={() => {
                             router.push(link)
                         }} text="Learn" />
