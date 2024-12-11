77c6fe3c7bd045229840b95c95d36cb8c15466b3
udayempire <kuday0726@gmail.com>
2024-12-12 04:10:35 +0530

    added vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Michroma } from "next/font/google";
 import "./globals.css";
 import Link from "next/link"
+import { Analytics } from '@vercel/analytics/next';
 
 const michroma = Michroma({
   weight: "400", 
@@ -29,6 +30,7 @@ export default function RootLayout({
           Register For CodeBeta 2.0!!
         </Link>
         {children}
+        <Analytics/>
       </body>
     </html>
   );
